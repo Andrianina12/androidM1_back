4761b413cd30b5bc09914aa1a2ca9b8de9d51c11
Andrianina12 <72884478+Andrianina12@users.noreply.github.com>
2022-05-20 23:11:40 +0300

    Update Login.js

--- src/repository/Login.js
+++ src/repository/Login.js
@@ -11,7 +11,7 @@ exports.login = async function login(user) {
         var collection = dbo.collection("Users");
         user.motdepasse = crypto.createHash('md5').update(user.motdepasse).digest('hex')
         var result = await collection.find(user).toArray();
-        if(result.length == 0) response = {code: 401,  data: null, message: "Identifiant ou mot de passe invalide"};
+        if(result.length == 0) response = {code: 401,  data: {}, message: "Identifiant ou mot de passe invalide"};
         else {
             result[0].token = crypto.createHash('md5').update(user.email + new Date()).digest('hex');
             await collection.updateOne(user,{$set: result[0]});
@@ -22,7 +22,7 @@ exports.login = async function login(user) {
         // await  listDatabases(client);
  
     } catch (e) {
-        response = {code: 501,  data: null, message: e.message};
+        response = {code: 501,  data: {}, message: e.message};
         console.error(e);
     } finally {
         await client.close();
@@ -41,10 +41,10 @@ exports.verifyToken = async function verifyToken(token) {
         console.log('token', token)
         const result = await collection.find({token: token}).toArray();
         console.log('result', result);
-        if(result.length == 0) response = {code: 401,  data: null, message: "Veuillez vous connecter"};
+        if(result.length == 0) response = {code: 401,  data: {}, message: "Veuillez vous connecter"};
         else response = result;
     } catch(e) {
-        response = {code: 501,  data: null, message: e.message};
+        response = {code: 501,  data: {}, message: e.message};
         console.error(e);
     } finally {
         await client.close()
